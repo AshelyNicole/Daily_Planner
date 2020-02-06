fd8497b06c8e72e848360cbca0389b30ad60d173
Ashely <arumbaoa10@gmail.com>
2020-02-05 22:37:40 -0800

    stupid save button

--- Asset/JS/script.js
+++ Asset/JS/script.js
@@ -11,7 +11,16 @@ $("#clock").text(moment().format("LT"))
 $("#getDate").text(moment().format("MMMM Do YYYY"))
 
 //save to localStorage
-
+$("#btnSave").click(function(){
+    console.log("Save that ishh...")
+    var eventTime = $(".time").text(eventTime)
+        value = $(".inputText")
+        eventDiscription = $("#9am_Data, #10am_Data, #11am_Data, #12pm_Data, #1pm_Data, #2pm_Data, #3pm_Data, #4pm_Data, #5pm_Data").value
+    // console.log(eventTime)
+    console.log(eventDiscription)
+    localStorage.setItem(eventTime, eventDiscription)
+    console.log(localStorage)
+})
 
 
 
