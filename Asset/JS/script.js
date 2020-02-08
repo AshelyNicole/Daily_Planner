8a4b984050ea79939b2d2e7cc8f1d5f5ad3b279d
Ashely <arumbaoa10@gmail.com>
2020-02-07 16:09:37 -0800

    jquery solved

--- Asset/JS/script.js
+++ Asset/JS/script.js
@@ -1,41 +1,65 @@
 //Calendar app
+$(document).ready(function(){
 
-//Create text fields
-//Save to localStorage (javascript set items)
-//Find a way get from localStorage to display (even after refresh being able to capture data from localStorage)
-//optional **momentJS - library for capturing a moment in time. (read docs for that)** optional
-
-
-//date & time
-$("#clock").text(moment().format("LT"))
-$("#getDate").text(moment().format("MMMM Do YYYY"))
-
-//save to localStorage
-$("#btnSave").click(function(){
-    console.log("Save that ishh...")
-    var eventTime = $(".time").text(eventTime)
-        value = $(".inputText")
-        eventDiscription = $("#9am_Data, #10am_Data, #11am_Data, #12pm_Data, #1pm_Data, #2pm_Data, #3pm_Data, #4pm_Data, #5pm_Data").value
-    // console.log(eventTime)
-    console.log(eventDiscription)
-    localStorage.setItem(eventTime, eventDiscription)
-    console.log(localStorage)
-})
-
-
-
-
-
-
-
-
-
-//clear button
-$("#btnClear").click(function() {
-    console.log("are you sure?");
-    var clearEvents = confirm("Are you sure you'd like to clear all events?");
-    if (clearEvents == true){
-        events.remove($("input type=text, textarea"))
-        //events.empty()
+    //Create text fields
+    //Save to localStorage (javascript set items)
+    //Find a way get from localStorage to display (even after refresh being able to capture data from localStorage)
+    //optional **momentJS - library for capturing a moment in time. (read docs for that)** optional
+    
+    
+    //date & time
+    $("#clock").text(moment().format("LT"))
+    $("#getDate").text(moment().format("MMMM Do YYYY"))
+    function reRender(){
+        $(".time").each(function(index,element){
+            let time= $(element).text()
+            console.log(time)
+            let todo = localStorage.getItem(time)
+            console.log(todo)
+            $(element).parent().siblings("form").children("input").attr("value", todo)
+        })
     }
+    reRender()
+    
+    //save to localStorage
+    $("#btnSave").click(function(){
+        console.log(this)
+        console.log($(this))
+        console.log($(".time"))
+        console.log("Save that ishh...")
+        $(".time").each(function(index,element){
+            var eventTime = $(element).text()
+            var eventDiscription= $(element).parent().siblings("form").children("input").val() 
+            // value = $(".inputText")
+            // eventDiscription = $("#9am_Data, #10am_Data, #11am_Data, #12pm_Data, #1pm_Data, #2pm_Data, #3pm_Data, #4pm_Data, #5pm_Data").value
+            // console.log(eventTime)
+            //console.log(eventTime)
+            localStorage.setItem(eventTime, eventDiscription)
+            
+        })
+        console.log(localStorage)
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //clear button
+    $("#btnClear").click(function() {
+        var clearEvents = confirm("Are you sure you'd like to clear all events?");
+        if (clearEvents){
+            console.log(" you are sure");
+            $(".time").each(function(index,element){
+                let time= $(element).text()
+                console.log(time)
+                localStorage.setItem(time, "")
+                reRender()
+            })
+            
+        }
+    })
 })
